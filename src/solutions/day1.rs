ada382aaa970d61135d8b382db630b1220c45a30
joe.hendley <95080839+Joe-Hendley@users.noreply.github.com>
2022-12-07 09:52:33 +0000

    update d1p1 to 1 line solution

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,32 +1,19 @@
 use crate::Solution;
-
 #[derive(Copy, Clone)]
 pub struct Day1 {}
 
 impl Solution for Day1 {
     fn solve_part_1(&self, input: &String) -> i32 {
-        let mut max = 0;
-        let mut calories = 0;
-
-        let lines = input.split("\n");
-
-        for line in lines {
-            if line == "" {
-                max = compare_one(max, calories);
-                calories = 0
-            } else {
-                calories += line.parse::<i32>().unwrap();
-            }
-        }
-
-        max = compare_one(max, calories);
-        return max;
+        input
+            .split("\n\n")
+            .map(|e| e.split("\n").map(|c| c.parse::<i32>().unwrap()).sum())
+            .max()
+            .unwrap()
     }
 
     fn solve_part_2(&self, input: &String) -> i32 {
         let mut maximums = vec![0; 3];
         let mut calories = 0;
-
         let lines = input.split("\n");
 
         for line in lines {
@@ -40,13 +27,6 @@ impl Solution for Day1 {
         maximums = compare_multiple(maximums, calories);
         return maximums.iter().sum();
     }
-}
-
-fn compare_one(max: i32, calories: i32) -> i32 {
-    if calories > max {
-        return calories;
-    }
-    return max;
 }
 
 fn compare_multiple(mut maximums: Vec<i32>, num: i32) -> Vec<i32> {
